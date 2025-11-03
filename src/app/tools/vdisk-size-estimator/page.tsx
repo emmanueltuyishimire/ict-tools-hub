5b188b2055ce0b48c90930f82738e23902bf3bbb
THE EXTRA COIN <theextracoin@gmail.com>
2025-11-03 00:00:31 +0000

    studio-3964744229:~/studio{main}$ npm run build
    > nextn@0.1.0 build
    > N

--- src/app/tools/vdisk-size-estimator/page.tsx
+++ src/app/tools/vdisk-size-estimator/page.tsx
@@ -1,6 +1,4 @@
 
-'use client';
-
 import React from 'react';
 import { PageHeader } from '@/components/page-header';
 import { DiskUsageEstimator } from '../disk-usage-estimator/disk-usage-estimator';
