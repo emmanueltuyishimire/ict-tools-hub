9c11ea0c80d261e647ce2e5929ee2e20ce9e1602
THE EXTRA COIN <theextracoin@gmail.com>
2025-11-03 20:19:13 +0000

    <!-- Google tag (gtag.js) --> <script async src="https://www.googletagma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,6 +86,17 @@ export default function RootLayout({
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2LF3Z5PGR4"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2LF3Z5PGR4');
+          `}
+        </Script>
       </head>
       <body
         suppressHydrationWarning={true}
