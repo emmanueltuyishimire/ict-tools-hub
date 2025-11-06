7e5232ae2d8a09465daa5a81c05a15512356e724
THE EXTRA COIN <theextracoin@gmail.com>
2025-11-06 03:08:02 +0000

    use logo.png please

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" prefetch={false}>
-      <Image src="/ict/logo.png" alt="ICT Tools Hub Logo" width={36} height={36} />
+      <Image src="/logo.png" alt="ICT Tools Hub Logo" width={36} height={36} />
       <span className="text-xl font-semibold tracking-tight font-headline">
         ICT Tools Hub
       </span>
